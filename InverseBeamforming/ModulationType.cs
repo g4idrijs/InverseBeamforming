96de19ada4d10a31af269cd1976946cbb9eba2d1
Travis Rennich <travis.rennich@gmail.com>
2016-06-22 21:19:05 -0400

    Added code to let user track the progress of the simulation as it happens. The changes were made for both ideal filtering simulations as well as FIR filtering simulations

--- InverseBeamforming/ModulationType.cs
+++ InverseBeamforming/ModulationType.cs
@@ -4,7 +4,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reactive.Linq;
+using System.IO;
 
+/// <summary>
+/// Contains all the components necesary to perform simulations regaurding digital communications systems
+/// and inverse beamforming
+/// </summary>
 namespace InverseBeamforming
 {
 	/// <summary>
@@ -12,10 +18,97 @@ namespace InverseBeamforming
 	/// </summary>
 	public partial class Modulations
 	{
+
+		/// <summary>
+		/// Describes the current state of the simulation
+		/// </summary>
+		public struct IntermediateSimResults
+		{
+			/// <summary>
+			/// Total bit errors produced already
+			/// </summary>
+			public int TotalErrors;
+
+			/// <summary>
+			/// Number of bit errors during this iteration of the simulation
+			/// </summary>
+			public int NumberErrorsThisIteration;
+
+			/// <summary>
+			/// Total number of bits simulated so far
+			/// </summary>
+			public int TotalBitsSimulated;
+
+			/// <summary>
+			/// Bit error rate of the simulation so far
+			/// </summary>
+			public double BitErrorRate
+			{ get { return (double)TotalErrors / TotalBitsSimulated; } }
+
+			/// <summary>
+			/// Total errors remaining before the simulation is complete
+			/// </summary>
+			public int TotalErrorsRemaining;
+
+			/// <summary>
+			/// Percentage of the total errors required to end the simulation
+			/// </summary>
+			public double PercentErrorHad
+			{ get { return (double)TotalErrors / (TotalErrors + TotalErrorsRemaining); } }
+
+			/// <summary>
+			/// Constructs an instance of the IntermediateSimResults Structure
+			/// </summary>
+			/// <param name="totalErrors">Total number of error occured in the simulation</param>
+			/// <param name="numberErrorsThisIteration">Total number of errors in this iteration</param>
+			/// <param name="totalBitsSimulated">Total number of bits simulated so far</param>
+			/// <param name="totalErrorsRemaining">Total number of errors remaining</param>
+			public IntermediateSimResults(int totalErrors, int numberErrorsThisIteration, int totalBitsSimulated, int totalErrorsRemaining)
+			{
+				TotalErrors = totalErrors;
+				NumberErrorsThisIteration = numberErrorsThisIteration;
+				TotalBitsSimulated = totalBitsSimulated;
+				TotalErrorsRemaining = totalErrorsRemaining;
+			}
+		}
+
+		/// <summary>
+		/// Describes the final state of the simulation
+		/// </summary>
+		public struct FinalSimResults
+		{
+			/// <summary>
+			/// Total bit errors produced already
+			/// </summary>
+			public int TotalErrors;
+
+			/// <summary>
+			/// Total number of bits simulated so far
+			/// </summary>
+			public int TotalBitsSimulated;
+
+			/// <summary>
+			/// Bit error rate of the simulation so far
+			/// </summary>
+			public double BitErrorRate
+			{ get { return (double)TotalErrors / TotalBitsSimulated; } }
+
+			/// <summary>
+			/// Constructs an instance of the IntermediateSimResults Structure
+			/// </summary>
+			/// <param name="totalErrors">Total number of error occured in the simulation</param>
+			/// <param name="totalBitsSimulated">Total number of bits simulated so far</param>
+			public FinalSimResults(int totalErrors, int totalBitsSimulated)
+			{
+				TotalErrors = totalErrors;
+				TotalBitsSimulated = totalBitsSimulated;
+			}
+		}
+
 		/// <summary>
 		/// Defines an interface that allows multiple modulation types to be created
 		/// </summary>
-		public abstract class ModulationType : I_SpreadingCode
+		public abstract class ModulationType : I_SpreadingCode, IObservable<IntermediateSimResults>
 		{
 			/// <summary>
 			/// String containing the type of modulation used
@@ -202,11 +295,17 @@ namespace InverseBeamforming
 			protected byte[] _bitsToCommunicationsSymbols;
 
 			/// <summary>
+			/// Provides a list to hold the suscribers of simulation events
+			/// </summary>
+			List<IObserver<IntermediateSimResults>> observers;
+
+			/// <summary>
 			/// Creates a new instance of the ModulationType class with seed as the seed for the RNG
 			/// </summary>
 			/// <param name="seed">Seed to initialize the RNG</param>
 			public ModulationType(int seed, double carrierFrequency, int samplingRate, int samplesPerSymbol, double signalPower, double[] firCoefficients, int numberSymbolsPerWaveform, int M)
 			{
+				observers = new List<IObserver<IntermediateSimResults>>();
 				this._rng = new Random(seed);
 				this.CarrierFrequency = carrierFrequency;
 				this.SampleRate = samplingRate;
@@ -644,6 +743,288 @@ namespace InverseBeamforming
 				});
 				return bers;
 			}
+
+			/// <summary>
+			/// Provides a way to unsubscribe from the simulation updates
+			/// </summary>
+			private class Unsubscriber : IDisposable
+			{
+				/// <summary>
+				/// List of observers of the updates
+				/// </summary>
+				private List<IObserver<IntermediateSimResults>> _observers;
+				
+				/// <summary>
+				/// Observer of the updates
+				/// </summary>
+				private IObserver<IntermediateSimResults> _observer;
+
+				/// <summary>
+				/// Construct a new instance of the Unsubscriber class
+				/// </summary>
+				/// <param name="observers">List of observers of the simulation updates</param>
+				/// <param name="observer">New observer of the simulation updates</param>
+				public Unsubscriber(List<IObserver<IntermediateSimResults>> observers, IObserver<IntermediateSimResults> observer)
+				{
+					this._observers = observers;
+					this._observer = observer;
+				}
+
+				/// <summary>
+				/// Dispose of the observer from the list
+				/// </summary>
+				public void Dispose()
+				{
+					//If the list isn't null, remove the observer from the list
+					if (! (_observer==null))
+					{
+						_observers.Remove(_observer);
+					}
+				}
+			}
+
+			/// <summary>
+			/// Subscribe to the simulation updates
+			/// </summary>
+			/// <param name="observer">New oberver of the simulation updates</param>
+			/// <returns>An interface detailing how to unsubscribe from the updates</returns>
+			public IDisposable Subscribe(IObserver<IntermediateSimResults> observer)
+			{
+				//If the observer is not already in the list of observers, add it
+				if (!observers.Contains(observer))
+					observers.Add(observer);
+
+				//Return an Unsubscriber
+				return new Unsubscriber(observers, observer);
+			}
+
+			/// <summary>
+			/// Runs a simulation that reports its progress
+			/// </summary>
+			/// <param name="numberToGetWrongEventually">Number of bit errors to simulate to</param>
+			/// <param name="noisePower">Power of the AWGN added to the signals</param>
+			/// <returns>Final results of the simulation</returns>
+			public FinalSimResults RunSimpleSimulationObservable(int numberToGetWrongEventually, double noisePower)
+			{
+				byte[] inbits;
+				double[] waveform;
+				byte[] outbits;
+
+				int numberWrongThisIteration = 0;
+				int totalNumWrong = 0, totalBitsSimulated = 0;
+
+				foreach (var obs in observers)
+				{
+
+					(obs as IntermediateSimResultsReporter).OnStart();
+				}
+
+				//Loop while there haven't been enough bit estimation errors
+				while (totalNumWrong < numberToGetWrongEventually)
+					{
+						//Generate Random bits
+						inbits = GenerateRandomBits();
+
+						//Modulate the bits
+						waveform = ModulateBits(inbits);
+
+						//Add noise to the waveform
+						AdditiveWhiteGaussianNoiseNR(ref waveform, noisePower);
+
+						//Demodulate the waveform+noise
+						outbits = CorrelationReceiver(waveform);
+
+						//Get the total number of bits that were estimated wrongly
+						numberWrongThisIteration= numberDifferentBits(inbits, outbits);
+						totalNumWrong += numberWrongThisIteration;
+
+						//Update the total number of bits simulated
+						totalBitsSimulated += this._numberSymbolsPerWaveform;
+
+						if (numberWrongThisIteration!=0)
+						{
+							foreach (var obs in observers)
+							{
+								obs.OnNext(new IntermediateSimResults(totalNumWrong, numberWrongThisIteration, totalBitsSimulated, numberToGetWrongEventually - totalNumWrong));
+							}
+						}
+					}
+
+				//Simulation is done, inform the observers
+				foreach (var obs in observers)
+				{
+					obs.OnCompleted();
+				}
+
+				return new FinalSimResults( totalNumWrong, totalBitsSimulated);
+			}
+
+			/// <summary>
+			/// Runs a simulation that reports its progress
+			/// </summary>
+			/// <param name="numberToGetWrongEventually">Number of bit errors to simulate to</param>
+			/// <param name="noisePower">Power of the AWGN added to the signals</param>
+			/// <returns>Final results of the simulation</returns>
+			public FinalSimResults RunFIRFilterSimulationObservable(int numberToGetWrongEventually, double noisePower)
+			{
+				byte[] inbits;
+				double[] waveform;
+				byte[] outbits;
+
+				int numberWrongThisIteration = 0;
+				int totalNumWrong = 0, totalBitsSimulated = 0;
+
+				//Call the OnStart function for each observer
+				foreach (var obs in observers)
+				{
+					(obs as IntermediateSimResultsReporter).OnStart();
+				}
+				
+				//Loop while there haven't been enough bit estimation errors
+				while (totalNumWrong < numberToGetWrongEventually)
+				{
+					//Generate Random bits
+					inbits = GenerateRandomBits();
+
+					//Modulate the bits
+					waveform = ModulateBits(inbits);
+
+					//Add noise to the waveform
+					AdditiveWhiteGaussianNoiseNR(ref waveform, noisePower);
+
+					//Filter the waveform
+					waveform = FIR_Filter(waveform);
+
+					//Demodulate the waveform+noise
+					outbits = CorrelationReceiver(waveform);
+
+					//Get the total number of bits that were estimated wrongly
+					numberWrongThisIteration = numberDifferentBits(inbits, outbits);
+					totalNumWrong += numberWrongThisIteration;
+
+					//Update the total number of bits simulated
+					totalBitsSimulated += this._numberSymbolsPerWaveform;
+
+					//If there were any bit errors this iteration
+					if (numberWrongThisIteration != 0)
+					{
+						//Call the OnNext function for each observer
+						foreach (var obs in observers)
+						{
+							obs.OnNext(new IntermediateSimResults(totalNumWrong, numberWrongThisIteration, totalBitsSimulated, numberToGetWrongEventually - totalNumWrong));
+						}
+					}
+				}
+
+				//Simulation is done, inform the observers
+				foreach (var obs in observers)
+				{
+					obs.OnCompleted();
+				}
+
+				//Return the final simulation results
+				return new FinalSimResults(totalNumWrong, totalBitsSimulated);
+			}
+
+			/// <summary>
+			/// Defines a class that reports the results of the simulation in real time
+			/// </summary>
+			public class IntermediateSimResultsReporter : IObserver<IntermediateSimResults>
+			{
+				/// <summary>
+				/// Starting time of the simulation
+				/// </summary>
+				private DateTime startTime;
+				/// <summary>
+				/// Ending time of the simulation
+				/// </summary>
+				private DateTime endTime;
+
+				/// <summary>
+				/// Hold the information needed to unsubscribe from the simulation
+				/// </summary>
+				private IDisposable unsubscriber;
+
+				/// <summary>
+				/// Hold a filename to write the results simultanesouly
+				/// </summary>
+				private string _filename;
+
+				/// <summary>
+				/// Construct a new instance of the class with a file to write the results to
+				/// </summary>
+				/// <param name="filename">Name of the file to write the results to</param>
+				public IntermediateSimResultsReporter(string filename)
+				{
+					this._filename = filename;
+				}
+
+				/// <summary>
+				/// Subscribe to receive the updates from the simulation
+				/// </summary>
+				/// <param name="provider">Simulation OBservable</param>
+				public virtual void Subscribe(IObservable<IntermediateSimResults> provider)
+				{
+					//Provide information to unsubscribe from the observable
+					unsubscriber = provider.Subscribe(this);
+				}
+
+				/// <summary>
+				/// Unsubscribe
+				/// </summary>
+				public virtual void Unsubscribe()
+				{
+					unsubscriber.Dispose();
+				}
+
+				/// <summary>
+				/// Sets up the file to log the simulation
+				/// </summary>
+				public virtual void OnStart()
+				{
+					//Get the starting time
+					startTime = DateTime.Now;
+
+					//Write the start time to the simulation
+					File.WriteAllText(_filename, "Start of simulation. Time: " + startTime.ToLongDateString() + " " + startTime.ToLongTimeString() + Environment.NewLine);
+				}
+
+				/// <summary>
+				/// Function to be run when the simulation is done
+				/// </summary>
+				public virtual void OnCompleted()
+				{
+					//Get the end time of the simulation
+					endTime = DateTime.Now;
+
+					//Append some timing information to the end
+					File.AppendAllText(_filename, String.Format("Completed simulation at {0} {1}.\n", endTime.ToLongDateString(), endTime.ToLongTimeString()));
+					File.AppendAllText(_filename, "The simulation took: " + (endTime - startTime).ToString());
+				}
+
+				/// <summary>
+				/// Function gets called when there is an error in the simulation
+				/// </summary>
+				/// <param name="error"></param>
+				public virtual void OnError(Exception error)
+				{
+					endTime = DateTime.Now;
+					File.AppendAllText(_filename, String.Format("An error was encoutered at {0} {1}.\n\n{1}", endTime.ToLongDateString(), endTime.ToLongTimeString(),error.Message));
+				}
+
+				/// <summary>
+				/// Function gets called when an update to the simulation is pushed
+				/// </summary>
+				/// <param name="isr">Struct holding the intermediate simulation results</param>
+				public virtual void OnNext(IntermediateSimResults isr)
+				{
+					//Write the information to the log file
+					File.AppendAllText(_filename, String.Format("{0} {1} Total Errors: {2, 5}, Errors this iteration: {3, 5}, Total Bits Simulated: {4, 9}, Bit Error Rate: {5: 0.00}, Percent errors found: {6: 0.00}\n", DateTime.Now.ToLongDateString(), DateTime.Now.ToLongTimeString(), isr.TotalErrors, isr.NumberErrorsThisIteration, isr.TotalBitsSimulated, isr.BitErrorRate, isr.PercentErrorHad*100));
+				}
+				
+			}
+
+			 
 
 			//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 			//Spreading Codes
--- InverseBeamformingTests/MPSK_ModulationTests.cs
+++ InverseBeamformingTests/MPSK_ModulationTests.cs
@@ -13,7 +13,7 @@ namespace InverseBeamforming.Tests
 	[TestClass()]
 	public class MPSK_ModulationTests
 	{
-		private string _testFileDumpDirec = @"C:\Users\vikin_000\OneDrive\AFIT\Thesis\Code\C_Sharp\InvBeamLib\InverseBeamforming\TestFileDump\MPSK" + Path.DirectorySeparatorChar;
+		private string _testFileDumpDirec = @"C:\Users\vikin\OneDrive\AFIT\Thesis\Code\C_Sharp\InvBeamLib\InverseBeamforming\TestFileDump\MPSK" + Path.DirectorySeparatorChar;
 
 		[TestMethod()]
 		public void MPSK_ModulationTest()
@@ -31,6 +31,21 @@ namespace InverseBeamforming.Tests
 		}
 
 		[TestMethod()]
+		public void ModulateBits_GenerateWaveformFor_qa_ModulateBits_char_float_Test()
+		{
+			var seed = 40;
+			var sampleRate = 100;
+			var carrierFrequency = 10;
+			var samplesPerSymbol = 10;
+			var signalPower = 1;
+
+
+			var mpsk = new MPSK_Modulation(carrierFrequency, sampleRate, seed, samplesPerSymbol, signalPower, null, 2, 4, new double[] { 0, Math.PI / 4, Math.PI / 2, 3 * Math.PI / 4 }, new byte[] { 0, 1, 2, 3 });
+			double[] waveform = mpsk.ModulateBits(new byte[] { 0, 1 });
+			writeToCSV(waveform, "MPSK_ModulateBitsTest");
+		}
+
+		[TestMethod()]
 		public void DemodulateWaveformTest()
 		{
 			var sampleRate = 1000;
@@ -42,6 +57,62 @@ namespace InverseBeamforming.Tests
 			writeToCSV(inbits, "inbits");
 			writeToCSV(outbits, "outbits");
 			CollectionAssert.AreEqual(inbits, outbits);
+		}
+
+		[TestMethod()]
+		public void RunSimpleSimulationObservableTest()
+		{
+			var seed = 40;
+			var sampleRate = 1220160;
+			var carrierFrequency = 101680;
+			var samplesPerSymbol = 7680;
+			var signalPower = 1;
+			var mpsk = new MPSK_Modulation(carrierFrequency, sampleRate, seed, samplesPerSymbol, signalPower, null, 2, 4, new double[] { 0, Math.PI / 4, Math.PI / 2, 3 * Math.PI / 4 }, new byte[] { 0, 1, 2, 3 });
+			
+
+			string filename = "RunSimpleSimulationObservableTest";
+			filename = _testFileDumpDirec + filename + ".csv";
+			var obs = mpsk.Subscribe(new MPSK_Modulation.IntermediateSimResultsReporter(filename));
+			mpsk.RunSimpleSimulationObservable(500, 1700);
+			obs.Dispose();
+		}
+		
+		[TestMethod()]
+		public void RunFIRFilterSimulationObservableTest()
+		{
+			double[] coefs = { 0.0004918999, 0.0003199292, -0.0000000000, -0.0003633909, -0.0006312609, -0.0006698830, -0.0004061822, 0.0001195890, 0.0007335821, 0.0011668717, 0.0011566730, 0.0005795375, -0.0004436998, -0.0015362768, -0.0021818714, -0.0019451886, -0.0007111603, 0.0011751752, 0.0029643583, 0.0037731036, 0.0029846847, 0.0006097011, -0.0025792523, -0.0052531992, -0.0060437038, -0.0041851771, 0.0000000000, 0.0050347924, 0.0087507230, 0.0091632713, 0.0054278953, -0.0015509929, -0.0092006763, -0.0141371985, -0.0135491320, -0.0065803763, 0.0049027331, 0.0166053238, 0.0232167449, 0.0205322359, 0.0075140558, -0.0125643771, -0.0324946486, -0.0431063759, -0.0362982920, -0.0081217790, 0.0392103634, 0.0972196347, 0.1531611401, 0.1935823237, 0.2083155053, 0.1935823237, 0.1531611401, 0.0972196347, 0.0392103634, -0.0081217790, -0.0362982920, -0.0431063759, -0.0324946486, -0.0125643771, 0.0075140558, 0.0205322359, 0.0232167449, 0.0166053238, 0.0049027331, -0.0065803763, -0.0135491320, -0.0141371985, -0.0092006763, -0.0015509929, 0.0054278953, 0.0091632713, 0.0087507230, 0.0050347924, 0.0000000000, -0.0041851771, -0.0060437038, -0.0052531992, -0.0025792523, 0.0006097011, 0.0029846847, 0.0037731036, 0.0029643583, 0.0011751752, -0.0007111603, -0.0019451886, -0.0021818714, -0.0015362768, -0.0004436998, 0.0005795375, 0.0011566730, 0.0011668717, 0.0007335821, 0.0001195890, -0.0004061822, -0.0006698830, -0.0006312609, -0.0003633909, -0.0000000000, 0.0003199292, 0.0004918999 };
+			var seed = 40;
+			var sampleRate = 1220160;
+			var carrierFrequency = 101680;
+			var samplesPerSymbol = 7680;
+			var signalPower = 1;
+			var mpsk = new MPSK_Modulation(carrierFrequency, sampleRate, seed, samplesPerSymbol, signalPower, coefs, 2, 4, new double[] { 0, Math.PI / 4, Math.PI / 2, 3 * Math.PI / 4 }, new byte[] { 0, 1, 2, 3 });
+
+			string filename = "RunFIRFilterSimulationObservableTest";
+			filename = _testFileDumpDirec + filename + ".csv";
+			var obs = mpsk.Subscribe(new MPSK_Modulation.IntermediateSimResultsReporter(filename));
+			mpsk.RunFIRFilterSimulationObservable(500, 1700);
+			obs.Dispose();
+		}
+
+
+		private void writeToCSV<T>(List<T> list, string filename, double sampleRate=0)
+		{
+
+			filename = _testFileDumpDirec + filename + ".csv";
+			var csv = new StringBuilder();
+
+			if (sampleRate != 0)
+				csv.AppendLine(sampleRate.ToString());
+
+			foreach (var item in list)
+			{
+				csv.AppendLine(item.ToString() + ",");
+			}
+			csv.Remove(csv.Length - 1, 1);
+
+			File.WriteAllText(filename, csv.ToString());
+			
 		}
 
 		private void writeToCSV<T>(T[] mat, string filename, double sampleRate = 0)
